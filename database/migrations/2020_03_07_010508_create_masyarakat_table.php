ed7e1e76a31f1d000b4b0d072678f65cfae4c1de
ravialdo <ravialdo@yahoo.com>
2020-10-14 10:48:58 +0700

    update scripts

--- database/migrations/2020_03_07_010508_create_masyarakat_table.php
+++ database/migrations/2020_03_07_010508_create_masyarakat_table.php
@@ -19,7 +19,7 @@ class CreateMasyarakatTable extends Migration
                   $table->string('username', 25)->unique();
                   $table->string('password', 255);
                   $table->string('telepon', 13)->unique();
-                  $table->enum('level', ['masyarakat']);
+                  $table->enum('level', ['masyarakat'])->default('masyarakat');
                   $table->timestamps();
             });
       }
--- resources/views/components/form-register.blade.php
+++ resources/views/components/form-register.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="{{ asset('publicimg/stisla-light.svg') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+              <img src="{{ asset('public/img/stisla-light.svg') }}" alt="logo" width="100" class="shadow-light rounded-circle">
             </div>
 
             <div class="card card-primary">
