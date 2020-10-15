dc9f4ab3ce5ea3113a1e82587b477b57d8648b6f
ravialdo <ravialdo@yahoo.com>
2020-10-15 21:58:58 +0700

    delete link image report

--- resources/views/layouts/laporan.blade.php
+++ resources/views/layouts/laporan.blade.php
@@ -110,7 +110,7 @@
 
       <!-- header -->
       <div class="text-center">
-            <img src="{{ public_path('/img/West_Java_coa.png') }}" class="img" alt="logo.png" width="90">
+            
             <div style="margin-left:6rem;">
                   <span class="text-header text-bold text-danger">
                         PEMERINTAH DAERAH PROVINSI JAWA BARAT <br> DINAS PENDIDIKAN <br>
